d67f03af636f0e56c90f74a3830c17af77484cd4
Michail Pishchagin <mblsha@yandex-team.ru>
2014-03-23 15:47:55 +0400

    Add Homebrew formula.

---
+++ dist-clang.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class DistClang < Formula
+  homepage 'https://github.com/mblsha/dist-clang'
+  head "https://github.com/mblsha/dist-clang.git"
+
+  depends_on "ninja"
+
+  def install
+    system "./src/build/gyp", "-Dversion=#{version}"
+    system "ninja", "-C", "src/out/Release"
+
+    prefix.install "src/out/Release/clang", "src/out/Release/clangd"
+    prefix.install Dir["src/out/Release/*.dylib"]
+  end
+
+  def caveats; <<-EOS.undent
+    Installed to /usr/local/opt/dist-clang
+    EOS
+  end
+end
